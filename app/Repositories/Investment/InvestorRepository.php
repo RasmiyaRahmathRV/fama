61d07788eb546130c7741eb53e7282e35d87ce67
Geethufama <geethufama@gmail.com>
2025-12-18 13:08:38 +0400

    investor Repo

--- app/Repositories/Investment/InvestorRepository.php
+++ app/Repositories/Investment/InvestorRepository.php
@@ -105,7 +105,7 @@ class InvestorRepository
 
     public function getInvestorsWithDetails()
     {
-        $investors = Investor::with(['payoutBatch', 'investorBanks', 'hasReferrer'])
+        $investors = Investor::with('payoutBatch', 'investorBanks')
             ->where('status', 1)
             ->get();
 
