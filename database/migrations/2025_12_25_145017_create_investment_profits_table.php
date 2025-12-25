eda25bfb3424009137650cbf79f84c49e9b1c5eb
Geethufama <geethufama@gmail.com>
2025-12-25 15:53:46 +0400

    migrations and models

--- database/migrations/2025_12_25_145017_create_investment_profits_table.php
+++ database/migrations/2025_12_25_145017_create_investment_profits_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('investment_payouts', function (Blueprint $table) {
+        Schema::create('investor_payouts', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('investment_id');
             $table->unsignedBigInteger('investor_id');
@@ -33,6 +33,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('investment_payouts');
+        Schema::dropIfExists('investor_payouts');
     }
 };
