3ed8bb0cba356075556c30cddbf6d7097db1a0f4
crmfama2025 <crmfama2025@gmail.com>
2025-12-31 09:46:56 +0400

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,7 +22,7 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             UserSeeder::class,
-            Industryseeder::class,
+            IndustrySeeder::class,
             CompanySeeder::class,
             LookupTablesSeeder::class,
             PaymentModeSeeder::class,
