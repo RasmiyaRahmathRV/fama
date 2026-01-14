7ccdb31f7988025cea6aca7c6c5024d83eff845f
Geethufama <geethufama@gmail.com>
2026-01-14 10:54:20 +0400

    cron test

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -17,13 +17,15 @@ class Kernel extends ConsoleKernel
         // $schedule->command('profit:update-monthly-pending')
         //     ->monthlyOn(1, '00:05')
         //     ->withoutOverlapping();
-        $schedule->command('profit:update-monthly-pending')->dailyAt('10:46')->withoutOverlapping()->before(function () {
-            \Log::info('profit:update-monthly-pending command started');
-        })
+        $schedule->command('profit:update-monthly-pending')
+            ->dailyAt('11:00')
+            ->withoutOverlapping()
+            ->before(function () {
+                \Log::info('profit:update-monthly-pending command started');
+            })
             ->after(function () {
                 \Log::info('profit:update-monthly-pending command finished');
-            });;
-        \Log::info('profit:update-monthly-pending command finished');
+            });
     }
 
     /**
