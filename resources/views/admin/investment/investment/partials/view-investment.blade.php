f009494437be4fdbe40994a0f89338ca09cf5991
Geethufama <geethufama@gmail.com>
2026-01-20 10:27:00 +0400

    reinvestment details added in investment view

--- resources/views/admin/investment/investment/partials/view-investment.blade.php
+++ resources/views/admin/investment/investment/partials/view-investment.blade.php
@@ -1,182 +1,193 @@
-<div class="card card-outline card-info">
-    <div class="card-header">
-        <h3 class="card-title text-teal text-bold">
-            <i class="fas fa-file-invoice-dollar mr-2"></i> Investment Details
-        </h3>
-    </div>
+ @if ($investment->parentInvestment)
+     <div class="alert alert-default-info ">
+         <strong>Reinvestment</strong><br>
+         Parent Investment:
+         <a href="{{ route('investment.show', $investment->parentInvestment->id) }}" class="text-bold text-info">
+             #{{ $investment->parentInvestment->investment_code }}
+         </a>
+     </div>
+ @endif
+ <div class="card card-outline card-info">
 
-    <div class="card-body p-0">
-        <div class="table-responsive">
-            <table class="table table-bordered table-striped ">
-                <thead class="bg-light">
-                    <tr>
-                        {{-- <th>#</th> --}}
-                        <th>Investment Code</th>
-                        <th>Investment Date</th>
-                        <th>Investment Amount</th>
-                        <th>Received</th>
-                        <th>Pending</th>
-                        <th>Profit</th>
-                        <th>Status</th>
-                    </tr>
-                </thead>
 
-                <tbody>
-                    <tr>
-                        <td>{{ $investment->investment_code }}</td>
-                        <td>{{ getFormattedDate($investment->investment_date) }}</td>
-                        <td>{{ number_format($investment->investment_amount, 2) }}</td>
-                        <td class="text-success">
-                            {{ number_format($investment->total_received_amount, 2) }}
-                        </td>
-                        <td class="text-danger">
-                            {{ number_format($investment->balance_amount, 2) }}
-                        </td>
-                        <td class="text-info">
-                            {{ number_format($investment->profit_amount, 2) }}
-                        </td>
-                        <td>
-                            @if ($investment->investment_status == 1)
-                                <span class="badge badge-success">Active</span>
-                            @else
-                                <span class="badge badge-secondary">Closed</span>
-                            @endif
-                        </td>
-                    </tr>
+     <div class="card-header">
+         <h3 class="card-title text-teal text-bold">
+             <i class="fas fa-file-invoice-dollar mr-2"></i> Investment Details
+         </h3>
+     </div>
 
-                </tbody>
-            </table>
-        </div>
+     <div class="card-body p-0">
+         <div class="table-responsive">
+             <table class="table table-bordered table-striped ">
+                 <thead class="bg-light">
+                     <tr>
+                         {{-- <th>#</th> --}}
+                         <th>Investment Code</th>
+                         <th>Investment Date</th>
+                         <th>Investment Amount</th>
+                         <th>Received</th>
+                         <th>Pending</th>
+                         <th>Profit</th>
+                         <th>Status</th>
+                     </tr>
+                 </thead>
 
-    </div>
+                 <tbody>
+                     <tr>
+                         <td>{{ $investment->investment_code }}</td>
+                         <td>{{ getFormattedDate($investment->investment_date) }}</td>
+                         <td>{{ number_format($investment->investment_amount, 2) }}</td>
+                         <td class="text-success">
+                             {{ number_format($investment->total_received_amount, 2) }}
+                         </td>
+                         <td class="text-danger">
+                             {{ number_format($investment->balance_amount, 2) }}
+                         </td>
+                         <td class="text-info">
+                             {{ number_format($investment->profit_amount, 2) }}
+                         </td>
+                         <td>
+                             @if ($investment->investment_status == 1)
+                                 <span class="badge badge-success">Active</span>
+                             @else
+                                 <span class="badge badge-secondary">Closed</span>
+                             @endif
+                         </td>
+                     </tr>
 
-</div>
-<div class="card card-outline card-info">
-    <div class="card-header">
-        <h3 class="card-title text-teal text-bold">
-            <i class="fas fa-file-invoice-dollar mr-2"></i> Profit Release Details
-        </h3>
-    </div>
+                 </tbody>
+             </table>
+         </div>
 
-    <div class="card-body p-0">
-        <div class="table-responsive">
-            <table class="table table-bordered table-striped ">
-                <thead class="bg-light">
-                    <tr>
-                        {{-- <th>#</th> --}}
-                        <th>Profit Release Due on</th>
-                        <th>Current Month Pending</th>
+     </div>
 
-                    </tr>
-                </thead>
+ </div>
+ <div class="card card-outline card-info">
+     <div class="card-header">
+         <h3 class="card-title text-teal text-bold">
+             <i class="fas fa-file-invoice-dollar mr-2"></i> Profit Release Details
+         </h3>
+     </div>
 
-                <tbody>
-                    <tr>
-                        <td>
-                            <span class="badge badge-light text-sm">
-                                {{ getFormattedDate($investment->next_profit_release_date) }}
-                            </span>
-                        </td>
-                        <td>
-                            <span class="badge badge-light text-sm text-danger">
-                                {{ number_format($investment->outstanding_profit, 2) }}
-                            </span>
-                        </td>
+     <div class="card-body p-0">
+         <div class="table-responsive">
+             <table class="table table-bordered table-striped ">
+                 <thead class="bg-light">
+                     <tr>
+                         {{-- <th>#</th> --}}
+                         <th>Profit Release Due on</th>
+                         <th>Current Month Pending</th>
 
-                    </tr>
+                     </tr>
+                 </thead>
 
-                </tbody>
-            </table>
-        </div>
+                 <tbody>
+                     <tr>
+                         <td>
+                             <span class="badge badge-light text-sm">
+                                 {{ getFormattedDate($investment->next_profit_release_date) }}
+                             </span>
+                         </td>
+                         <td>
+                             <span class="badge badge-light text-sm text-danger">
+                                 {{ number_format($investment->outstanding_profit, 2) }}
+                             </span>
+                         </td>
 
-    </div>
+                     </tr>
 
-</div>
-<div class="card card-outline card-info">
-    <div class="card-header">
-        <h3 class="card-title text-teal text-bold">
-            <i class="fas fa-user mr-2"></i> Nominee Details
-        </h3>
-    </div>
+                 </tbody>
+             </table>
+         </div>
 
-    <div class="card-body p-0">
-        <div class="table-responsive">
-            <table class="table table-bordered table-striped">
-                <thead class="bg-light">
-                    <tr>
-                        {{-- <th>#</th> --}}
-                        <th>Nominee Name</th>
-                        <th>Nominee Email</th>
-                        <th>Nominee Phone</th>
+     </div>
 
-                    </tr>
-                </thead>
+ </div>
+ <div class="card card-outline card-info">
+     <div class="card-header">
+         <h3 class="card-title text-teal text-bold">
+             <i class="fas fa-user mr-2"></i> Nominee Details
+         </h3>
+     </div>
 
-                <tbody>
-                    <tr>
-                        <td>{{ $investment->nominee_name ?? ' - ' }}</td>
-                        <td>{{ $investment->nominee_email ?? ' - ' }}</td>
-                        <td>{{ $investment->nominee_phone ?? ' - ' }}</td>
-                    </tr>
+     <div class="card-body p-0">
+         <div class="table-responsive">
+             <table class="table table-bordered table-striped">
+                 <thead class="bg-light">
+                     <tr>
+                         {{-- <th>#</th> --}}
+                         <th>Nominee Name</th>
+                         <th>Nominee Email</th>
+                         <th>Nominee Phone</th>
 
-                </tbody>
-            </table>
-        </div>
-    </div>
+                     </tr>
+                 </thead>
 
-</div>
+                 <tbody>
+                     <tr>
+                         <td>{{ $investment->nominee_name ?? ' - ' }}</td>
+                         <td>{{ $investment->nominee_email ?? ' - ' }}</td>
+                         <td>{{ $investment->nominee_phone ?? ' - ' }}</td>
+                     </tr>
 
-<div class="card card-outline card-info">
-    <div class="card-header">
-        <h3 class="card-title text-teal text-bold">
-            <i class="fas fa-file-alt mr-2"></i> Investment Document
-        </h3>
-    </div>
+                 </tbody>
+             </table>
+         </div>
+     </div>
 
-    <div class="card-body p-0">
-        <div class="table-responsive">
-            <table class="table table-bordered table-striped">
-                <thead class="bg-light">
-                    <tr>
-                        <th>Uploaded On</th>
-                        <th>Type</th>
-                        <th>File</th>
-                    </tr>
-                </thead>
+ </div>
 
-                <tbody>
-                    @if ($document)
-                        <tr>
-                            <td>
-                                {{ $document->created_at?->format('d M Y') ?? '-' }}
-                            </td>
-                            <td>Contract File</td>
+ <div class="card card-outline card-info">
+     <div class="card-header">
+         <h3 class="card-title text-teal text-bold">
+             <i class="fas fa-file-alt mr-2"></i> Investment Document
+         </h3>
+     </div>
 
-                            <td>
-                                @if (!empty($document->investment_contract_file_path))
-                                    <a href="{{ asset('storage/' . $document->investment_contract_file_path) }}"
-                                        target="_blank" class="btn btn-xs btn-primary">
-                                        <i class="fas fa-eye"></i> view
-                                    </a>
+     <div class="card-body p-0">
+         <div class="table-responsive">
+             <table class="table table-bordered table-striped">
+                 <thead class="bg-light">
+                     <tr>
+                         <th>Uploaded On</th>
+                         <th>Type</th>
+                         <th>File</th>
+                     </tr>
+                 </thead>
 
-                                    <a href="{{ asset('storage/' . $document->investment_contract_file_path) }}"
-                                        download class="btn btn-xs btn-success">
-                                        <i class="fas fa-download"></i>Download
-                                    </a>
-                                @endif
+                 <tbody>
+                     @if ($document)
+                         <tr>
+                             <td>
+                                 {{ $document->created_at?->format('d M Y') ?? '-' }}
+                             </td>
+                             <td>Contract File</td>
 
-                            </td>
-                        </tr>
-                    @else
-                        <tr>
-                            <td colspan="5" class="text-center text-muted">
-                                <i class="fas fa-exclamation-triangle"></i>
-                                No document found for this investment
-                            </td>
-                        </tr>
-                    @endif
-                </tbody>
-            </table>
-        </div>
-    </div>
-</div>
+                             <td>
+                                 @if (!empty($document->investment_contract_file_path))
+                                     <a href="{{ asset('storage/' . $document->investment_contract_file_path) }}"
+                                         target="_blank" class="btn btn-xs btn-primary">
+                                         <i class="fas fa-eye"></i> view
+                                     </a>
+
+                                     <a href="{{ asset('storage/' . $document->investment_contract_file_path) }}"
+                                         download class="btn btn-xs btn-success">
+                                         <i class="fas fa-download"></i>Download
+                                     </a>
+                                 @endif
+
+                             </td>
+                         </tr>
+                     @else
+                         <tr>
+                             <td colspan="5" class="text-center text-muted">
+                                 <i class="fas fa-exclamation-triangle"></i>
+                                 No document found for this investment
+                             </td>
+                         </tr>
+                     @endif
+                 </tbody>
+             </table>
+         </div>
+     </div>
+ </div>
